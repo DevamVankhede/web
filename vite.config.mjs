08e1332085eb6173c48a7184205d36c479d2f0dd
devam <devamvankhede05@gmail.com>
2025-10-03 18:13:47 +0530

    commit

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
   server: {
@@ -7,6 +8,14 @@ export default defineConfig({
   build: {
     outDir: "../dist",
     emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "src/index.html"),
+        membership: resolve(__dirname, "src/membership.html"),
+        aboutus: resolve(__dirname, "src/aboutus.html"),
+      },
+    },
   },
   root: "src",
+  publicDir: ".",
 });
